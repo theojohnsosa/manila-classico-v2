553d5f038266e4362531ef5867f54fc11df2e8c5
Theojohn Sosa <theojohnsosa@gmail.com>
2025-08-28 23:33:14 +0800

    cleaned up Main class

--- manila-classico-v2/src/manila/classico/v2/Main.java
+++ manila-classico-v2/src/manila/classico/v2/Main.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package manila.classico.v2;
 
-/**
- *
- * @author theojohnsosa
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         LandingPage landingPage = new LandingPage();
