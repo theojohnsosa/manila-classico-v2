c35e19a18921296467a4285e900b3b24e570804a
Theojohn Sosa <theojohnsosa@gmail.com>
2025-08-29 00:58:30 +0800

    cleaned up Service

--- manila-classico-v2/src/manila/classico/v2/Service.java
+++ manila-classico-v2/src/manila/classico/v2/Service.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package manila.classico.v2;
 
-/**
- *
- * @author theojohnsosa
- */
 public class Service {
     private String name;
     private double price;
