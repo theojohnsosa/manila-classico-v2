98f48f1e5a8afa424bd3a611be79429116c6822b
Theojohn Sosa <theojohnsosa@gmail.com>
2025-08-29 00:59:07 +0800

    cleaned up ServiceManager

--- manila-classico-v2/src/manila/classico/v2/ServiceManager.java
+++ manila-classico-v2/src/manila/classico/v2/ServiceManager.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package manila.classico.v2;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author theojohnsosa
- */
 public class ServiceManager {
     private static final List<Service> services = new ArrayList<>();
 
