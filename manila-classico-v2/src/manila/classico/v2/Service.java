6778f80094d12eaa666bdca4566056ba4b5b82cb
Theojohn Sosa <theojohnsosa@gmail.com>
2025-09-13 18:00:35 +0800

    refactored variables in service class

--- manila-classico-v2/src/manila/classico/v2/Service.java
+++ manila-classico-v2/src/manila/classico/v2/Service.java
@@ -1,19 +1,19 @@
 package manila.classico.v2;
 
 public class Service {
-    private String name;
-    private double price;
+    private String serviceName;
+    private double servicePrice;
 
-    public Service(String name, double price) {
-        this.name = name;
-        this.price = price;
+    public Service(String serviceName, double servicePrice) {
+        this.serviceName = serviceName;
+        this.servicePrice = servicePrice;
     }
 
     public String getName() {
-        return name;
+        return serviceName;
     }
 
     public double getPrice() {
-        return price;
+        return servicePrice;
     }
 }
