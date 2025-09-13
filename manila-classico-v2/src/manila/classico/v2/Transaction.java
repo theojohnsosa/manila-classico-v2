7e37e191e97c5b82bbc9d8b7e533473042b771eb
Theojohn Sosa <theojohnsosa@gmail.com>
2025-09-13 21:57:53 +0800

    refactored variables in transaction class

--- manila-classico-v2/src/manila/classico/v2/Transaction.java
+++ manila-classico-v2/src/manila/classico/v2/Transaction.java
@@ -2,22 +2,22 @@ package manila.classico.v2;
 
 public class Transaction {
     private final String customerName;
-    private final String contact;
-    private final String service;
-    private final String barber;
-    private final String date;
-    private final String time;
-    private final String paymentMethod;
+    private final String customerContact;
+    private final String chosenService;
+    private final String chosenBarber;
+    private final String chosenDate;
+    private final String chosenTime;
+    private final String chosenPaymentMethod;
     private final String paymentAmount;
 
-    public Transaction(String customerName, String contact, String service, String barber, String date, String time, String paymentMethod, String paymentAmount) {
+    public Transaction(String customerName, String customerContact, String chosenService, String chosenBarber, String chosenDate, String chosenTime, String chosenPaymentMethod, String paymentAmount) {
         this.customerName = customerName;
-        this.contact = contact;
-        this.service = service;
-        this.barber = barber;
-        this.date = date;
-        this.time = time;
-        this.paymentMethod = paymentMethod;
+        this.customerContact = customerContact;
+        this.chosenService = chosenService;
+        this.chosenBarber = chosenBarber;
+        this.chosenDate = chosenDate;
+        this.chosenTime = chosenTime;
+        this.chosenPaymentMethod = chosenPaymentMethod;
         this.paymentAmount = paymentAmount;
     }
 
@@ -26,27 +26,27 @@ public class Transaction {
     }
     
     public String getContact() { 
-        return contact; 
+        return customerContact; 
     }
     
     public String getService() { 
-        return service; 
+        return chosenService; 
     }
     
     public String getBarber() { 
-        return barber; 
+        return chosenBarber; 
     }
     
     public String getDate() { 
-        return date; 
+        return chosenDate; 
     }
     
     public String getTime() { 
-        return time; 
+        return chosenTime; 
     }
     
     public String getPaymentMethod() { 
-        return paymentMethod; 
+        return chosenPaymentMethod; 
     }
     
     public String getPaymentAmount() { 
