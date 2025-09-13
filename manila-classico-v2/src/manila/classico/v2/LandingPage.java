958d5af5e4317f3d301f643b4743b9e4decc4a8a
Theojohn Sosa <theojohnsosa@gmail.com>
2025-09-13 11:02:56 +0800

    removed logger in landing page

--- manila-classico-v2/src/manila/classico/v2/LandingPage.java
+++ manila-classico-v2/src/manila/classico/v2/LandingPage.java
@@ -1,10 +1,6 @@
 package manila.classico.v2;
 
-import java.util.logging.Logger;
-
 public class LandingPage extends javax.swing.JFrame {
-    
-    private static final Logger logger = Logger.getLogger(LandingPage.class.getName());
 
     public LandingPage() {
         initComponents();
@@ -255,10 +251,6 @@ public class LandingPage extends javax.swing.JFrame {
     }//GEN-LAST:event_staffLoginButtonActionPerformed
 
     public static void main(String args[]) {
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -267,11 +259,14 @@ public class LandingPage extends javax.swing.JFrame {
                 }
             }
         } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
+//            logger.log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
-        java.awt.EventQueue.invokeLater(() -> new LandingPage().setVisible(true));
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AddServicePage().setVisible(true);
+            }
+        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
