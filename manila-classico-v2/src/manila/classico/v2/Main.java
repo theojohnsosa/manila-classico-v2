941515e053a8341017588fad9e43233abb7d1583
Theojohn Sosa <theojohnsosa@gmail.com>
2025-09-18 01:20:35 +0800

    cleaned up main class v2

--- manila-classico-v2/src/manila/classico/v2/Main.java
+++ manila-classico-v2/src/manila/classico/v2/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ViewQueuePage viewQueuePage = new ViewQueuePage();
+        viewQueuePage.setVisible(true);
+        
         LandingPage landingPage = new LandingPage();
         landingPage.setLocationRelativeTo(null);
         landingPage.setResizable(false);
         landingPage.setVisible(true);
-        
-        ViewQueuePage viewQueuePage = new ViewQueuePage();
-        viewQueuePage.setVisible(true);
         
     }
     
